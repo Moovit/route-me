6034a6c8b052912ecc25ac98e1336e1175c63c00
Gil Shapira <gshapira@gmail.com>
2013-03-27 17:06:57 +0200

    o Allow applications to set which logger to use for RMLog calls

--- MapView/Map/RMLogging.h
+++ MapView/Map/RMLogging.h
@@ -1,6 +1,6 @@
 //
-// RouteMe.h
-// 
+// RMLogging.h
+//
 // Copyright (c) 2008-2013, Route-Me Contributors
 // All rights reserved.
 //
@@ -25,50 +25,19 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-// The list of header files for more convenient Route-Me import to projects.
-// (in alphabetic order)
-
-#import "NSUserDefaults+RouteMe.h"
-#import "RMAbstractMercatorTileSource.h"
-#import "RMAbstractWebMapSource.h"
-#import "RMAnnotation.h"
-#import "RMBingSource.h"
-#import "RMCacheObject.h"
-#import "RMCircle.h"
-#import "RMConfiguration.h"
-#import "RMCompositeSource.h"
-#import "RMCoordinateGridSource.h"
-#import "RMDBMapSource.h"
-#import "RMDatabaseCache.h"
-#import "RMFoundation.h"
-#import "RMFractalTileProjection.h"
-#import "RMGenericMapSource.h"
-#import "RMGlobalConstants.h"
 #import "RMLogger.h"
-#import "RMLoadingTileView.h"
-#import "RMMapLayer.h"
-#import "RMMapOverlayView.h"
-#import "RMMapQuestOSMSource.h"
-#import "RMMapQuestOpenAerialSource.h"
-#import "RMMapScrollView.h"
-#import "RMMapTiledLayerView.h"
-#import "RMMapView.h"
-#import "RMMapViewDelegate.h"
-#import "RMMarker.h"
-#import "RMMemoryCache.h"
-#import "RMOpenCycleMapSource.h"
-#import "RMOpenSeaMapLayer.h"
-#import "RMOpenSeaMapSource.h"
-#import "RMOpenStreetMapSource.h"
-#import "RMPixel.h"
-#import "RMProjection.h"
-#import "RMQuadTree.h"
-#import "RMShape.h"
-#import "RMTile.h"
-#import "RMTileCache.h"
-#import "RMTileCacheDownloadOperation.h"
-#import "RMTileImage.h"
-#import "RMTileSource.h"
-#import "RMTileSourcesContainer.h"
-#import "RMUserLocation.h"
-#import "RMUserTrackingBarButtonItem.h"
+
+#if DEBUG
+
+#define RMLog(args...)      do { if (RMLogger) RMLogger([NSString stringWithFormat:args]); } while (0)
+#define LogMethod()         RMLog(@"logged method call: -[%@ %@] (line %d)", self, NSStringFromSelector(_cmd), __LINE__)
+#define WarnDeprecated()    RMLog(@"***** WARNING: deprecated method call: -[%@ %@] (line %d)", self, NSStringFromSelector(_cmd), __LINE__)
+
+#else // DEBUG
+
+#define RMLog(args...)
+#define LogMethod()
+#define WarnDeprecated()
+#define NS_BLOCK_ASSERTIONS 1
+
+#endif // DEBUG
